ba5045612c74b11bd403cb18089310add7567b86
Dreadfang <jannekarhu2@kao.fi>
2017-01-20 21:57:44 +0200

    Spawner thingymabob
    spawner spawnin crap

--- Assets/scripts/Spawner.cs
+++ Assets/scripts/Spawner.cs
@@ -5,28 +5,79 @@ public class Spawner : MonoBehaviour {
 
 	public GameObject[] enemyArray;
 	public Vector3 spawnValues;
-
-
+	public int spawnCounter;
+	public int waveCounter = 1;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+	public bool spawning = false;
+	public int waveProgress;
 	// Use this for initialization
 	void Start () {
-		StartCoroutine (SpawnWaves ());
+		StartCoroutine (SpawnWave ());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 
-	IEnumerator SpawnWaves ()
+	IEnumerator SpawnWave ()
 	{
-		yield return new WaitForSeconds (1);
-		while (true) 
+		yield return new WaitForSeconds (startWait);
+		spawning = true;
+		while (spawning == true) 
 		{
 			Vector3 spawnPos = new Vector3 (-spawnValues.x, spawnValues.y, spawnValues.z);
 			Quaternion spawnRotation = Quaternion.identity;
-			Instantiate (enemyArray[1], spawnPos, spawnRotation);
-			yield return new WaitForSeconds (1);
+			if (waveCounter == 1) 
+			{
+				if (spawnCounter < 8)
+					Instantiate (enemyArray [0], spawnPos, spawnRotation);
+				
+			}
+			if (waveCounter == 2) 
+			{
+				if (spawnCounter < 6)
+					Instantiate (enemyArray [0], spawnPos, spawnRotation);
+				if (spawnCounter > 5 && spawnCounter < 8)
+					Instantiate (enemyArray [1], spawnPos, spawnRotation);
+			}
+			if (waveCounter == 3) 
+			{
+				if (spawnCounter < 4)
+					Instantiate (enemyArray [0], spawnPos, spawnRotation);
+				if (spawnCounter > 3 && spawnCounter < 7)
+					Instantiate (enemyArray [1], spawnPos, spawnRotation);
+				if (spawnCounter == 7)
+					Instantiate (enemyArray [2], spawnPos, spawnRotation);
+			}
+			if (waveCounter == 4) 
+			{
+				if (spawnCounter < 6)
+					Instantiate (enemyArray [1], spawnPos, spawnRotation);
+				if (spawnCounter > 5 && spawnCounter < 8)
+					Instantiate (enemyArray [2], spawnPos, spawnRotation);
+			}
+			if (waveCounter == 5) 
+			{
+				if (spawnCounter < 5)
+					Instantiate (enemyArray [1], spawnPos, spawnRotation);
+				if (spawnCounter > 4 && spawnCounter < 8)
+					Instantiate (enemyArray [2], spawnPos, spawnRotation);
+			}
+			spawnCounter ++ ;
+			Debug.Log (spawnCounter);
+			yield return new WaitForSeconds (spawnWait);
+
+			if (waveProgress == 8) 
+			{
+				spawnCounter = 0;
+				waveCounter++;
+				waveProgress = 0;
+			}
 		}
-		yield return new WaitForSeconds (20);
+		yield return new WaitForSeconds (waveWait);
+		spawning = true;
 	}
 }
