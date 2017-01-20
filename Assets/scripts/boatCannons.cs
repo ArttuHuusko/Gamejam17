920707dbe51d4b625e9a3bc1f74948f16740d81e
ArttuHuusko <FGJ2016@kamit.fi>
2017-01-20 20:55:12 +0200

    first commit

---
+++ Assets/scripts/boatCannons.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boatCannons : MonoBehaviour {
+
+    public KeyCode CannonShoot = KeyCode.T;
+    public GameObject cannonBall;
+
+	// Use this for initialization
+	void Start () 
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKeyDown(CannonShoot))
+        {
+            Instantiate(cannonBall, this.transform.position, Quaternion.identity);
+        }
+	}
+}
---
+++ Assets/scripts/cannonBall.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cannonBall : MonoBehaviour
+{
+    bool flyRight;
+
+    public GameObject boat;
+    public float ballSpeed = 10f;
+    public Rigidbody2D body;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Rigidbody2D body = GetComponent( typeof(Rigidbody2D)) as Rigidbody2D;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (flyRight == true)
+        {
+            body.AddRelativeForce(Vector2.right * ballSpeed);
+        }
+        else
+        {
+            body.AddRelativeForce(Vector2.left * ballSpeed);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "cannonRight")
+        {
+            flyRight = true;
+        }
+    }
+}
+
+
+
