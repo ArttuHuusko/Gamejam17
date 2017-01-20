b472adc6388bf30af4be1e8a3398eaa4c247af7e
ArttuHuusko <FGJ2016@kamit.fi>
2017-01-21 01:48:41 +0200

    AI initial push,

--- Assets/scripts/AIMovement.cs
+++ Assets/scripts/AIMovement.cs
@@ -1,19 +1,93 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class AIMovement : MonoBehaviour {
 
+    float distanceFromPlayer;
+    float angleToPlayer;
+
+    public Vector2 aPosition1 = new Vector2(3, 3);
+
+    //Quaternion playerRotation = Quaternion.identity;
+
+    public GameObject player;
+    public Rigidbody2D body;
+    public float Speed = 10f;
     public float maxSpeed = 10;
 
 	// Use this for initialization
 	void Start () 
     {
-	    
+       body = GetComponent<Rigidbody2D>();
+
+       //VAIHDA JOS PELAAJAN NIMI VAIHTUU SCENESSÄ
+       //recommended -> player = GameObject.FindWithTag("player");
+       player = GameObject.Find("boat");
+       
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () 
     {
-	    
+
+        float distanceFromPlayer = Vector2.Distance(player.transform.position, transform.position);
+
+        Vector3 dir = player.transform.position - transform.position;
+        dir = player.transform.InverseTransformDirection(dir);
+        float angleToPlayer = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+       // Debug.Log(dir);
+        //Debug.Log(angleToPlayer);
+
+        Debug.Log(distanceFromPlayer);
+
+        if (distanceFromPlayer > 50f)
+        {
+            ApproachPlayer();
+        }
+        else
+        {
+            TurnToShootingPosition();
+        }
+
+        if(body.velocity.magnitude > maxSpeed)
+        {
+            body.velocity = body.velocity.normalized * maxSpeed;
+        }
+
+        body.AddRelativeForce(Vector2.up * Speed);
+
+       //if (shootingPosChosen == true)
+       //{
+       //    chosenPlayerShootingPos 
+       //}
 	}
+
+    //works as intended as far as i know
+    void ApproachPlayer()
+    {
+        Vector3 diff = player.transform.position - transform.position;
+        diff.Normalize();
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+        //transform.rotation = Vector2.MoveTowards(transform.position, player.transform.position, Speed);
+    }
+
+    //make it so the boat turns slowly this function does not differ from void InShootingPosition() right now 
+    void TurnToShootingPosition()
+    {
+        Quaternion playerRotation = player.transform.rotation;
+        transform.rotation = Quaternion.Lerp(transform.rotation, playerRotation, 1);
+    }
+
+    void InShootingPosition()
+    {
+        Quaternion rotation = Quaternion.LookRotation
+            (player.transform.position - transform.position, transform.TransformDirection(Vector3.up));
+        transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+    }
+
+   
+
 }
---
+++ Assets/scripts/AIMovementOld.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIMovementOld : MonoBehaviour {
+
+
+    public Vector2 playerShootingPosLeftBack;
+    public Vector2 playerShootingPosRightBack;
+    public Vector2 playerShootingPosLeftFront;
+    public Vector2 playerShootingPosRightFront;
+
+    Vector2 chosenVector;
+
+    bool shootingPosChosen = false;
+
+    float closestPoint;
+
+    //Vector2[] shootingPositionArray;
+
+    public Vector2 chosenPlayerShootingPos;
+
+    public GameObject player;
+    public Rigidbody2D body;
+    public float Speed = 10;
+    public float maxSpeed = 10;
+
+    // Use this for initialization
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+
+        //VAIHDA JOS PELAAJAN NIMI VAIHTUU SCENESSÄ
+        //recommended -> player = GameObject.FindWithTag("player");
+        player = GameObject.Find("boat");
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        InitPlayerShootingPositions();
+
+        if (body.velocity.magnitude > maxSpeed)
+        {
+            body.velocity = body.velocity.normalized * maxSpeed;
+        }
+
+        body.AddRelativeForce(Vector2.up * Speed);
+
+        //if (shootingPosChosen == true)
+        //{
+        //    chosenPlayerShootingPos 
+        //}
+    }
+
+    void InitPlayerShootingPositions()
+    {
+
+        playerShootingPosLeftBack = new Vector2(player.transform.position.x - 30f, player.transform.position.y - 30f);
+
+        playerShootingPosLeftFront = new Vector2(player.transform.position.x - 30f, player.transform.position.y + 30f);
+
+        playerShootingPosRightBack = new Vector2(player.transform.position.x + 30f, player.transform.position.y - 30f);
+
+        playerShootingPosRightFront = new Vector2(player.transform.position.x + 30f, player.transform.position.y + 30f);
+
+        float posLB = Vector2.Distance(playerShootingPosLeftBack, transform.position);
+
+        float posLF = Vector2.Distance(playerShootingPosLeftFront, transform.position);
+
+        float posRB = Vector2.Distance(playerShootingPosRightBack, transform.position);
+
+        float posRF = Vector2.Distance(playerShootingPosRightFront, transform.position);
+
+        closestPoint = Mathf.Min(posLB, posLF, posRB, posRF);
+
+        if (closestPoint == posLB)
+        {
+            chosenVector = playerShootingPosLeftBack;
+        }
+        else if (closestPoint == posLF)
+        {
+            chosenVector = playerShootingPosLeftFront;
+        }
+        else if (closestPoint == posRB)
+        {
+            chosenVector = playerShootingPosRightBack;
+        }
+        else if (closestPoint == posRF)
+        {
+            chosenVector = playerShootingPosRightFront;
+        }
+
+        Debug.Log(chosenVector);
+
+
+
+        //DELET THIS
+        //List <Vector2> shootingPositionList = new List<Vector2>();
+        //    shootingPositionList.Add(new Vector2(player.transform.position.x - 30f, player.transform.position.y - 30f));
+        //    shootingPositionList.Add(new Vector2(player.transform.position.x - 30f, player.transform.position.y + 30f));
+        //    shootingPositionList.Add(new Vector2(player.transform.position.x + 30f, player.transform.position.y - 30f));
+        //    shootingPositionList.Add(new Vector2(player.transform.position.x + 30f, player.transform.position.y + 30f));
+        //    ChosenPlayerShootingPos == position AI will move towards to reach shooting range on player
+        //    Vector2 AIPathFindTarget = shootingPositionList[Random.Range(0, shootingPositionList.Count)];
+        //    Debug.Log(chosenPlayerShootingPos);
+        //DELET THIS
+
+
+
+
+
+
+    }
+
+    //FIND BETTER FUNCTION NAME
+    void AIBeginAttack()
+    {
+        shootingPosChosen = true;
+    }
+
+}
