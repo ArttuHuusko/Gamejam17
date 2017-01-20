c091f57975954b1299b3f99eee05826e19097f38
Aapo Ritvanen <aapo.ritvanen@gmail.com>
2017-01-21 00:43:07 +0200

    Drag n Drop
    :hand:

---
+++ Assets/scripts/ballDragger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class ballDragger : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler 
+{
+	Vector3 startPos;
+	public Camera mainCamera;
+
+	public void OnBeginDrag (PointerEventData eventData)
+	{
+		startPos = transform.position;
+		GetComponent<CanvasGroup>().blocksRaycasts = false;
+	}
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		transform.position = mainCamera.ScreenToWorldPoint (eventData.position) + Vector3.forward;
+		GetComponent<Image>().color = Color.white;
+	}
+
+	public void OnEndDrag(PointerEventData eventData)
+	{
+		transform.position = startPos;
+		GetComponent<CanvasGroup>().blocksRaycasts = true;
+	}
+}
---
+++ Assets/scripts/sideCannonDrag.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class sideCannonDrag : MonoBehaviour, IDropHandler
+{
+	public void OnDrop(PointerEventData eventData)
+	{
+		GetComponent<Image> ().color = Random.ColorHSV ();
+	}
+}
