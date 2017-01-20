447d7c602a5f19f5b03a7de757381ffafdf2c2b1
ArttuHuusko <FGJ2016@kamit.fi>
2017-01-20 22:06:50 +0200

    aapo käski committata sudoku

---
+++ Assets/scripts/AIMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIMovement : MonoBehaviour {
+
+    public float maxSpeed = 10;
+
+	// Use this for initialization
+	void Start () 
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+	    
+	}
+}
