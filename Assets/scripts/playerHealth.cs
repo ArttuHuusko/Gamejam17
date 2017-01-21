e2bd90c0aa41e1031555368d43414a46d459de82
Dreadfang <jannekarhu2@kao.fi>
2017-01-21 12:27:05 +0200

    Our :ship:  be sinkin'

---
+++ Assets/scripts/playerHealth.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerHealth : MonoBehaviour {
+
+	public int health = 5;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (health <= 0) 
+		{
+			Debug.Log ("We be sinking mateys!! Abandon ship!");
+			health = 0;
+		}
+	}
+	void OnCollisionEnter2D (Collision2D coll)
+	{
+		if (coll.gameObject.tag == ("enemy")) 
+		{
+			health--;
+			Debug.Log ("We be hit sire");
+		}
+	}
+}
