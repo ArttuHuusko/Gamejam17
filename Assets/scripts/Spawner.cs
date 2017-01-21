f336581f5ac9b64183a8cb2ae5ccb00b8235897e
Dreadfang <jannekarhu2@kao.fi>
2017-01-22 01:06:50 +0200

    Spawner and stats

--- Assets/scripts/Spawner.cs
+++ Assets/scripts/Spawner.cs
@@ -13,6 +13,7 @@ public class Spawner : MonoBehaviour {
 	public float waveWait;
 	public bool spawning = false;
 	public int waveProgress;
+	public int currentWave;
 	public Transform[] spawnPoints;
 	public int i;
 	// Use this for initialization
@@ -36,46 +37,52 @@ public class Spawner : MonoBehaviour {
 		{
 			i = Random.Range(0,3);
 			Transform pos = spawnPoints[i];
+			Quaternion spawnRotation = Quaternion.identity;
 			if (waveCounter == 1) 
 			{
-				if (spawnCounter < 8)
+				currentWave = 2;
+				if (spawnCounter < 2)
 					Instantiate (enemyArray [0], pos.position, pos.rotation);
 				
 			}
 			if (waveCounter == 2) 
 			{
-				if (spawnCounter < 6)
+				currentWave = 4;
+				if (spawnCounter < 4)
 					Instantiate (enemyArray [0], pos.position, pos.rotation);
-				if (spawnCounter > 5 && spawnCounter < 8)
-					Instantiate (enemyArray [1], pos.position, pos.rotation);
+			/*	if (spawnCounter > 5 && spawnCounter < 8)
+					Instantiate (enemyArray [1], pos.position, pos.rotation);	*/
 			}
 			if (waveCounter == 3) 
 			{
-				if (spawnCounter < 4)
+				currentWave = 6;
+				if (spawnCounter < 6)
 					Instantiate (enemyArray [0], pos.position, pos.rotation);
-				if (spawnCounter > 3 && spawnCounter < 7)
+			/*	if (spawnCounter > 3 && spawnCounter < 7)
 					Instantiate (enemyArray [1], pos.position, pos.rotation);
 				if (spawnCounter == 7)
-					Instantiate (enemyArray [2], pos.position, pos.rotation);
+					Instantiate (enemyArray [2], pos.position, pos.rotation);	*/
 			}
 			if (waveCounter == 4) 
 			{
-				if (spawnCounter < 6)
+				currentWave = 8;
+				if (spawnCounter < 8)
 					Instantiate (enemyArray [1], pos.position, pos.rotation);
-				if (spawnCounter > 5 && spawnCounter < 8)
-					Instantiate (enemyArray [2], pos.position, pos.rotation);
+			/*	if (spawnCounter > 5 && spawnCounter < 8)
+					Instantiate (enemyArray [2], pos.position, pos.rotation); */
 			}
 			if (waveCounter == 5) 
 			{
-				if (spawnCounter < 5)
+				currentWave = 10;
+				if (spawnCounter < 10)
 					Instantiate (enemyArray [1], pos.position, pos.rotation);
-				if (spawnCounter > 4 && spawnCounter < 8)
-					Instantiate (enemyArray [2], pos.position, pos.rotation);
+			/*	if (spawnCounter > 4 && spawnCounter < 8)
+					Instantiate (enemyArray [2], pos.position, pos.rotation); */
 			}
 			spawnCounter ++ ;
 			yield return new WaitForSeconds (spawnWait);
 
-			if (waveProgress == 8) 
+			if (waveProgress == currentWave) 
 			{
 				spawnCounter = 0;
 				waveCounter++;
---
+++ Assets/scripts/statScene.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class statScene : MonoBehaviour {
+
+	public GameObject TopWall;
+	public GameObject MidWall;
+	public GameObject BotWall;
+
+	public GameObject TopLeftCannon;
+	public GameObject MidLeftCannon;
+	public GameObject BotLeftCannon;
+	public GameObject TopRightCannon;
+	public GameObject MidRightCannon;
+	public GameObject BotRightCannon;
+
+	public playerHealth TopHealth;
+	public playerHealth MidHealth;
+	public playerHealth BotHealth;
+
+	public boatCannons TopLeftLoaded;
+	public boatCannons MidLeftLoaded;
+	public boatCannons BotLeftLoaded;
+	public boatCannons TopRightLoaded;
+	public boatCannons MidRightLoaded;
+	public boatCannons BotRightLoaded;
+
+	public Image TopLeftSprite;
+	public Image MidLeftSprite;
+	public Image BotLeftSprite;
+	public Image TopRightSprite;
+	public Image MidRightSprite;
+	public Image BotRightSprite;
+
+	public Sprite Loaded;
+	public Sprite Unloaded;
+
+	public Text TopHp;
+	public Text MidHp;
+	public Text BotHp;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		TopHealth = TopWall.GetComponent<playerHealth>();
+		MidHealth = MidWall.GetComponent<playerHealth>();
+		BotHealth = BotWall.GetComponent<playerHealth>();
+		TopLeftLoaded = TopLeftCannon.GetComponent<boatCannons> ();
+		MidLeftLoaded = MidLeftCannon.GetComponent<boatCannons> ();
+		BotLeftLoaded = BotLeftCannon.GetComponent<boatCannons> ();
+		TopRightLoaded = TopRightCannon.GetComponent<boatCannons> ();
+		MidRightLoaded = MidRightCannon.GetComponent<boatCannons> ();
+		BotRightLoaded = BotRightCannon.GetComponent<boatCannons> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		TopHp.text = TopHealth.health.ToString();
+		MidHp.text = MidHealth.health.ToString();
+		BotHp.text = BotHealth.health.ToString();
+
+		if (TopLeftLoaded.cannonLoaded == true) 
+		{
+			TopLeftSprite.sprite = Loaded;
+		} 
+		else 
+		{
+			TopLeftSprite.sprite = Unloaded;
+		}
+		if (MidLeftLoaded.cannonLoaded == true) 
+		{
+			MidLeftSprite.sprite = Loaded;
+		}
+		else 
+		{
+			MidLeftSprite.sprite = Unloaded;
+		}
+		if (BotLeftLoaded.cannonLoaded == true) 
+		{
+			BotLeftSprite.sprite = Loaded;
+		}
+		else 
+		{
+			BotLeftSprite.sprite = Unloaded;
+		}
+		if (TopRightLoaded.cannonLoaded == true) 
+		{
+			TopRightSprite.sprite = Loaded;
+		}
+		else 
+		{
+			TopRightSprite.sprite = Unloaded;
+		}
+		if (MidRightLoaded.cannonLoaded == true) 
+		{
+			MidRightSprite.sprite = Loaded;
+		}
+		else 
+		{
+			MidRightSprite.sprite = Unloaded;
+		}
+		if (BotRightLoaded.cannonLoaded == true) 
+		{
+			BotRightSprite.sprite = Loaded;
+		}
+		else 
+		{
+			BotRightSprite.sprite = Unloaded;
+		}
+	}
+}
