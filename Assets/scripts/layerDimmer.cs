62758ef7e7c55cde6375c372f6764446a0983956
Aapo Ritvanen <aapo.ritvanen@gmail.com>
2017-01-21 22:46:13 +0200

    Drag and Drop v3? D:

---
+++ Assets/scripts/layerDimmer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class layerDimmer : MonoBehaviour 
+{
+	GameObject[] layerChildren;
+	float layerDistanceCopy;
+
+	void Start () 
+	{
+		layerDistanceCopy = transform.parent.gameObject.GetComponent<sideLayerManager> ().layerDistance;
+
+		layerChildren = new GameObject[transform.childCount];
+		for (int c = 0; c < layerChildren.Length; c++) 
+			layerChildren [c] = transform.GetChild (c).gameObject;
+
+		for (int c = 0; c < layerChildren.Length; c++) 
+			Debug.Log (layerChildren[c]);
+	}
+
+	void Update () 
+	{
+		//for (int c = 0; c < layerChildren.Length; c++) 
+		//{
+			// v = 3000 -> 0, v = -3000 -> 0, v= 0 -> 1
+			//layerChildren[c].GetComponent<Image> ().color = new Color (1, 1, 1, 
+		//}
+	}
+}
--- Assets/scripts/sideLayerManager.cs
+++ Assets/scripts/sideLayerManager.cs
@@ -1,15 +1,55 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class sideLayerManager : MonoBehaviour {
 
-	void Start () 
+	bool isMoving = false;
+	float currentVelocity;
+	float startPos;
+	float currentPos;
+	float smoothZ;
+	int nextPositionMinus = 1;
+	public float animationSpeed = 1f;
+	public float layerDistance = 3000f;
+	public float nextPosition;
+
+	void Start()
 	{
-	
+		//nextPosition = transform.localPosition.z;
 	}
 
 	void Update () 
 	{
-	
+
+		if (Input.mouseScrollDelta.y != 0f && isMoving == false)
+		{
+			isMoving = true;
+			startPos = transform.localPosition.z;
+			currentPos = 0f;
+			nextPositionMinus = 1;
+			if (Input.mouseScrollDelta.y != 0) 
+				nextPosition = transform.localPosition.z + layerDistance;
+			if (Input.mouseScrollDelta.y > 0)
+				nextPositionMinus = -1;
+		}
+
+		if (isMoving) 
+		{
+			currentPos += Time.deltaTime * animationSpeed;
+			//float smoothZ = Mathf.SmoothDamp (transform.localPosition.z, nextPosition, ref currentVelocity, animationSpeed);
+			smoothZ = smootherstep(startPos, nextPosition, currentPos);
+			transform.localPosition = new Vector3 (transform.localPosition.x, transform.localPosition.y, smoothZ * nextPosition * nextPositionMinus);
+			if (smoothZ == 1f)
+				isMoving = false;
+		}
+	}
+
+	float smootherstep(float edge0, float edge1, float x)
+	{
+		// Scale, and clamp x to 0..1 range
+		x = Mathf.Clamp((x - edge0)/(edge1 - edge0), 0.0f, 1.0f);
+		// Evaluate polynomial
+		return x*x*x*(x*(x*6 - 15) + 10);
 	}
 }
