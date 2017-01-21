228b570e72809ae0c60fb8b80d93a691f45e96e2
ArttuHuusko <FGJ2016@kamit.fi>
2017-01-21 15:45:29 +0200

    AI works
    check TODO line on AIMovement.cs

---
+++ Assets/scripts/AICannons.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AICannons : MonoBehaviour {
+
+    public GameObject cannonBall;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    public void FireCannons()
+    {
+        Instantiate(cannonBall, this.transform.position, transform.parent.rotation);
+    }
+}
--- Assets/scripts/AIMovement.cs
+++ Assets/scripts/AIMovement.cs
@@ -4,13 +4,20 @@ using System.Collections.Generic;
 
 public class AIMovement : MonoBehaviour {
 
+    //TODO: TWEAK NUMBERS, MAKE SO AI BOATS DONT COLLIDE
+
     float distanceFromPlayer;
     float angleToPlayer;
     float rotationDiff;
     float t;
-
     public float engageTimer = 5.0f;
     float engageTimerCopy;
+    public float inFiringPosition = 6f;
+    float inFiringPositionCopy;
+    public float shootDelay = 0.5f;
+    float shootDelayCopy;
+    public float reloadingTime = 5;
+    public float ammo = 1;
 
     Vector3 currentRotation;
     Vector3 currentRotationRet;
@@ -20,10 +27,11 @@ public class AIMovement : MonoBehaviour {
     Quaternion targetRotation;
 
     bool approaching = false;
-    bool retreating = false;
+    public bool retreating = false;
     public bool facingPlayer = false;
-
-    //Quaternion playerRotation = Quaternion.identity;
+    public bool shootDelayTime = false;
+    bool reloading = false;
+    public bool fire = false;
 
     public GameObject player;
     public Rigidbody2D body;
@@ -31,9 +39,15 @@ public class AIMovement : MonoBehaviour {
     public float maxSpeed = 10;
     public float maxTurnRate = 0.5f;
 
+    public GameObject cannonBall;
+
+
 	// Use this for initialization
 	void Start () 
     {
+       shootDelayCopy = shootDelay;
+       //AICannonsScript = GetComponentsInChildren<AICannons>();
+       inFiringPositionCopy = inFiringPosition;
        engageTimerCopy = engageTimer;
        body = GetComponent<Rigidbody2D>();
        player = GameObject.FindWithTag("player");
@@ -52,6 +66,7 @@ public class AIMovement : MonoBehaviour {
         dir = player.transform.InverseTransformDirection(dir);
         float angleToPlayer = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
 
+        //called after AI shoots or runs out of inFiringPositionTime
         if (retreating == true)
         {
             Vector3 diffAway = player.transform.position - transform.position;
@@ -60,13 +75,20 @@ public class AIMovement : MonoBehaviour {
             rot_zAway = rot_zAway + 180;
             transform.rotation = Quaternion.Euler(0f, 0f, rot_zAway - 90);
 
-            //float playerRotationRet = player.transform.localEulerAngles.z;
             Vector3 rotationDiff = transform.rotation.eulerAngles; //possibly useless check when avaliable
 
             currentRotationRet = transform.eulerAngles;
             currentRotationRet.z = Mathf.Lerp(currentRotation.z, rot_zAway, maxTurnRate * Time.deltaTime);
             transform.eulerAngles = currentRotationRet;
 
+            shootDelay -= Time.fixedDeltaTime;
+
+            if (ammo > 0)
+            {
+                StartCoroutine(Firing());
+            }
+
+            //starts logic from the beginning, and resets all timers
             if (engageTimer > 0)
             {
                 engageTimer = engageTimer - Time.fixedDeltaTime;
@@ -74,10 +96,13 @@ public class AIMovement : MonoBehaviour {
             else if (engageTimer <= 0)
             {
                 retreating = false;
+                shootDelay = shootDelayCopy;
+                ammo += 1;
+                inFiringPosition = inFiringPositionCopy;
                 engageTimer = engageTimerCopy;
             }
         }
-        else if (distanceFromPlayer > 50f)
+        else if (distanceFromPlayer > 50f) //<--- change number possibly
         {
             BeginEngaging();
         }
@@ -85,6 +110,18 @@ public class AIMovement : MonoBehaviour {
         {
             TurnToShootingPosition();
         }
+
+        if (shootDelayTime == true)
+        {
+            shootDelay -= Time.fixedDeltaTime;
+
+        }
+        if (reloadingTime > 0)
+        {
+            shootDelayTime = false;
+            //shootDelay = shootDelayCopy;
+        }
+
 
         
 
@@ -102,48 +139,54 @@ public class AIMovement : MonoBehaviour {
 
 	}
 
-    //moves boat straight towards player
+    //turns boat when it is close to player
     void BeginEngaging()
     {
-
+        inFiringPosition -= Time.fixedDeltaTime;
         var newRotation = Quaternion.LookRotation(transform.position - player.transform.position, Vector3.forward);
         newRotation.x = 0.0f;
         newRotation.y = 0.0f;
         transform.rotation = Quaternion.RotateTowards(transform.rotation, newRotation, maxTurnRate);
 
     }
-
-    //make it so the boat turns slowly this function does not differ from void InShootingPosition() right now 
     void TurnToShootingPosition()
     {
+        //calculates angles and differences between player and AIboat
+        //may have useless stuff check later
         facingPlayer = false;
         float playerRotation = player.transform.localEulerAngles.z;
         Vector3 rotationDiff = transform.rotation.eulerAngles;
-        //rotationDiff = playerRotation.z - transform.rotation.z;
         currentRotation = transform.eulerAngles;
         currentRotation.z = Mathf.Lerp(currentRotation.z, playerRotation, 0.2f * Time.deltaTime);
         transform.eulerAngles = currentRotation;
-        //transform.rotation = Quaternion.Lerp(transform.rotation, playerRotation, 1);
 
         //InShootingPosition();
     }
 
-    //never called right now
-    void InShootingPosition()
+    public void OnTriggerEnter2D(Collider2D other)
     {
-        Quaternion rotation = Quaternion.LookRotation
-            (player.transform.position - transform.position, transform.TransformDirection(Vector3.up));
-        transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
+        
         if (other.tag == "player")
         {
-            //Fire after like half a second or smth.
-            //also make failsafe if trigger doesnt hit so AI doesnt get stuck
+            shootDelayTime = true;
+            retreating = true;
+            //StartCoroutine(Reload());
+        }
+        if (inFiringPosition <= 0)
+        {
             retreating = true;
         }
+    }
+
+    IEnumerator Firing()
+    {
+        ammo -= 1;
+        yield return new WaitForSeconds(0.3f);
+        Instantiate(cannonBall, transform.FindChild("cannon").transform.position, this.transform.rotation);
+        Instantiate(cannonBall, transform.FindChild("cannon (2)").transform.position, this.transform.rotation);
+        Instantiate(cannonBall, transform.FindChild("cannon (3)").transform.position, this.transform.rotation);
+        Instantiate(cannonBall, transform.FindChild("cannon (5)").transform.position, this.transform.rotation);
+        
     }
 
    
