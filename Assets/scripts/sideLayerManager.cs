7d878686fe01b044ba21615a698debfbdc7f5759
Aapo Ritvanen <aapo.ritvanen@gmail.com>
2017-01-22 13:57:05 +0200

    Shadows BRO!

--- Assets/scripts/sideLayerManager.cs
+++ Assets/scripts/sideLayerManager.cs
@@ -9,14 +9,17 @@ public class sideLayerManager : MonoBehaviour {
 	public float startPos;
 	public float currentPos;
 	public float smoothZ;
-	public int nextPositionMinus = 1;
 	public float animationSpeed = 1f;
 	public float layerDistance = 3000f;
 	public float nextPosition;
+	public float maxAlpha = 70f;
+
+	public Image botLayerShadow;
+	public Image topLayerShadow;
 
 	void Start()
 	{
-		
+		maxAlpha = maxAlpha / 255;
 	}
 
 	void Update () 
@@ -26,7 +29,6 @@ public class sideLayerManager : MonoBehaviour {
 			isMoving = true;
 			startPos = transform.localPosition.z;
 			currentPos = 0f;
-			nextPositionMinus = 1;
 			smoothZ = 0f;
 			if (Input.mouseScrollDelta.y < 0) 
 			{
@@ -47,9 +49,26 @@ public class sideLayerManager : MonoBehaviour {
 			currentPos += Time.deltaTime * animationSpeed;
 			smoothZ = smootherstep(0f, 1f, currentPos);
 			transform.localPosition = new Vector3 (transform.localPosition.x, transform.localPosition.y, nextPosition * smoothZ + startPos);
+			if (nextPosition + startPos < 0) 
+			{
+				botLayerShadow.color = new Color (0, 0, 0, (1 - smoothZ) * maxAlpha);
+			}
+			if (nextPosition + startPos > 0) 
+			{
+				topLayerShadow.color = new Color (0, 0, 0, smoothZ * maxAlpha);
+			}
+			if (nextPosition + startPos == 0) 
+			{
+				if (topLayerShadow.color == Color.clear)
+					botLayerShadow.color = new Color (0, 0, 0, smoothZ * maxAlpha);
+				if (topLayerShadow.color != Color.clear)
+					topLayerShadow.color = new Color (0, 0, 0, (1 - smoothZ) * maxAlpha);
+			}
 			if (smoothZ == 1f)
 				isMoving = false;
 		}
+
+
 	}
 
 	float smootherstep(float edge0, float edge1, float x)
